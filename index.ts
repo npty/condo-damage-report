17f79bdcaf35cb23659e9c3428bfdf6011d15d14
npty <nptytn2@gmail.com>
2025-04-17 10:03:57 +0700

    feat: use environment variable for port configuration

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import * as http from "http";
 import * as fs from "fs";
 import * as path from "path";
 
-const PORT: number = 3010;
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3010;
 // Assume index.html is in the same directory as index.ts
 const INDEX_HTML_PATH: string = path.join(__dirname, "index.html");
 
@@ -21,4 +21,3 @@ server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
   console.log(`Attempting to serve: ${INDEX_HTML_PATH}`);
 });
-
