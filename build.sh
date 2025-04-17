7ad3a6f3379f50945381f59b0f78fecc01d95895
npty <nptytn2@gmail.com>
2025-04-17 13:16:21 +0700

    build: serve static files from public directory

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ if [ -z "$BUILDING_NAME" ]; then
     BUILDING_NAME=""
 fi
 
-sed -i "s#\"%%BUILDING_NAME%%\"#\"$BUILDING_NAME\"#g" index.html
+mkdir -p public
+
+cp index.html public/index.html
+sed -i "s#\"%%BUILDING_NAME%%\"#\"$BUILDING_NAME\"#g" public/index.html
 
 echo "Attempted injection of BUILDING_NAME='$BUILDING_NAME' into index.html using multi-step sed"
 
