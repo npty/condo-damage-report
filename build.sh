94f517d0d9807cb65901f818099b6d841a22a273
npty <nptytn2@gmail.com>
2025-04-17 13:21:53 +0700

    chore: remove redundant sed command in build script

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ mkdir -p public
 
 cp index.html public/index.html
 sed -i "s#\"%%BUILDING_NAME%%\"#\"$BUILDING_NAME\"#g" public/index.html
-sed -i "s#\"%%BUILDING_NAME%%\"#\"$BUILDING_NAME\"#g" index.html
 
 echo "Attempted injection of BUILDING_NAME='$BUILDING_NAME' into index.html using multi-step sed"
 
